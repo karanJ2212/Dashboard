91c3f06260cc0eedddce36be9180e1e1ff79c5a4
karan jain <karanjain2212@gmail.com>
2023-09-24 20:47:26 +0530

    topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ColorModeContext, useMode } from './theme';
+import Topbar from './pages/global/Topbar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -8,7 +9,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content" />
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
