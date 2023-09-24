47d5b423afafb7c20f7664fcfede12a0d96c26fb
karan jain <karanjain2212@gmail.com>
2023-09-25 01:38:13 +0530

    created statbox component

---
+++ src/components/StatBox.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { Box, Typography, useTheme } from '@mui/material';
+import React from 'react';
+import { tokens } from '../theme';
+
+const StatBox = ({ title, subtitle, icon }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          <Typography
+            variant="h2"
+            fontWeight="bold"
+            sx={{ color: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            borderRadius: '50%',
+            width: '50px',
+            height: '50px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: colors.greenAccent[600],
+          }}
+        >
+          {icon}
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h4">{subtitle}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default StatBox;
