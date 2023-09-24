095c96afceed65d31b04f2e26cf76ab5153a1859
karan jain <karanjain2212@gmail.com>
2023-09-24 21:23:52 +0530

    defined routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { ColorModeContext, useMode } from './theme';
-import Topbar from './pages/global/Topbar';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./pages/global/Topbar";
+import Dashboard from "./pages/dashboard";
+import Messages from "./pages/messages";
+import Project from "./pages/project";
+import Staffing from "./pages/staffing";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,6 +16,12 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/Project" element={<Project />} />
+              <Route path="/Staffing" element={<Staffing />} />
+              <Route path="/Messages" element={<Messages />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ src/pages/staffing/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Staffing() {
+  return <div>staffing</div>;
+}
