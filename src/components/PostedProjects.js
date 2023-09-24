627cd24d9f7d39fcef7025d9b56179e4178deb7c
karan jain <karanjain2212@gmail.com>
2023-09-25 02:49:15 +0530

    linters

--- src/components/PostedProjects.js
+++ src/components/PostedProjects.js
@@ -1,43 +1,45 @@
-import { Box, Typography, useTheme, Paper } from "@mui/material";
-import React from "react";
-import { tokens } from "../theme";
+import {
+  Box, Typography, useTheme, Paper,
+} from '@mui/material';
+import React from 'react';
+import { tokens } from '../theme';
 
 const projects = [
   {
     id: 1,
 
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    status: "Resume Review",
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    status: 'Resume Review',
   },
   {
     id: 2,
 
-    description: "Nulla facilisi. Vestibulum condimentum vehicula lorem.",
-    status: "Interview",
+    description: 'Nulla facilisi. Vestibulum condimentum vehicula lorem.',
+    status: 'Interview',
   },
   {
     id: 3,
 
-    description: "Vestibulum vel est vitae enim congue euismod.",
-    status: "Task",
+    description: 'Vestibulum vel est vitae enim congue euismod.',
+    status: 'Task',
   },
   {
     id: 4,
 
-    description: "Fusce dignissim, elit eget efficitur malesuada.",
-    status: "Payment",
+    description: 'Fusce dignissim, elit eget efficitur malesuada.',
+    status: 'Payment',
   },
   {
     id: 5,
 
-    description: "Pellentesque habitant morbi tristique senectus.",
-    status: "Closed",
+    description: 'Pellentesque habitant morbi tristique senectus.',
+    status: 'Closed',
   },
   {
     id: 6,
 
-    description: "Aenean eget odio eu enim efficitur malesuada.",
-    status: "Payment",
+    description: 'Aenean eget odio eu enim efficitur malesuada.',
+    status: 'Payment',
   },
 ];
 
@@ -47,17 +49,17 @@ export default function PostedProjects() {
 
   const getStatusColor = (status) => {
     switch (status) {
-      case "Resume Review":
+      case 'Resume Review':
         return colors.redAccent[500];
-      case "Interview":
+      case 'Interview':
         return colors.redAccent[100];
-      case "Task":
+      case 'Task':
         return colors.greenAccent[500];
-      case "Payment":
+      case 'Payment':
         return colors.primary[100];
-      case "Closed":
+      case 'Closed':
         return colors.primary[100];
-      case "Paymentt":
+      case 'Paymentt':
         return colors.greenAccent[900];
       default:
         return colors.grey[100]; // Default color
@@ -69,9 +71,9 @@ export default function PostedProjects() {
       sx={{
         backgroundColor: colors.primary[400],
         p: 3,
-        borderRadius: "8px",
-        textAlign: "center",
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+        borderRadius: '8px',
+        textAlign: 'center',
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
         border: `1px solid ${colors.grey[200]}`,
       }}
     >
@@ -88,7 +90,7 @@ export default function PostedProjects() {
         <Typography variant="h4" component="p" color={colors.grey[100]}>
           <a
             href="/see-all"
-            style={{ textDecoration: "none", color: colors.grey[100] }}
+            style={{ textDecoration: 'none', color: colors.grey[100] }}
           >
             See All
           </a>
@@ -101,9 +103,9 @@ export default function PostedProjects() {
             key={project.id}
             sx={{
               backgroundColor: colors.primary[800],
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
               p: 2,
               my: 2,
             }}
@@ -113,12 +115,12 @@ export default function PostedProjects() {
                 {project.description}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <div
                 style={{
-                  width: "24px",
-                  height: "24px",
-                  borderRadius: "50%",
+                  width: '24px',
+                  height: '24px',
+                  borderRadius: '50%',
                   backgroundColor: getStatusColor(project.status),
                 }}
               />
