b24ca7c07a0c7f5b7391f4cff271d9551deb73f7
karan jain <karanjain2212@gmail.com>
2023-09-25 01:27:36 +0530

    added icons

--- src/pages/global/Sidebar.jsx
+++ src/pages/global/Sidebar.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
-// import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+
 import {
   Sidebar, Menu, MenuItem, sidebarClasses,
 } from 'react-pro-sidebar';
 import {
-  Avatar, Box, IconButton, Typography, useTheme,
+  Avatar, Box, Typography, useTheme,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
-// import "react-pro-sidebar/dist/css/styles.css";
+
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
-import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+
+import LayersIcon from '@mui/icons-material/Layers';
+import GroupIcon from '@mui/icons-material/Group';
+import ForumIcon from '@mui/icons-material/Forum';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
 
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
@@ -38,9 +39,6 @@ const Item = ({
         icon={icon}
       >
         <Typography>{title}</Typography>
-
-        {/* component=
-      <Link to={to} /> */}
       </MenuItem>
     </Link>
   );
@@ -101,22 +99,16 @@ const SideNavbar = () => {
                 alignItems="center"
                 ml="15px"
               >
-                {/* <Typography variant="h3" color={colors.grey[100]}>
-                  ADMINIS
-                </Typography> */}
                 <img
                   src={logo}
                   alt="Company Logo"
                   style={{
-                    width: '160px', // Adjust the width as needed
-                    height: '42px', // Adjust the height as needed to match the Typography
+                    width: '160px',
+                    height: '42px',
                     background: '#fff',
                     padding: '5px',
                   }}
                 />
-                {/* <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon />
-                </IconButton> */}
               </Box>
             )}
           </MenuItem>
@@ -133,21 +125,21 @@ const SideNavbar = () => {
             <Item
               title="Projects"
               to="/Project"
-              icon={<PeopleOutlinedIcon />}
+              icon={<LayersIcon />}
               selected={selected}
               setSelected={setSelected}
             />
             <Item
               title="Staffing"
               to="/Staffing"
-              icon={<ContactsOutlinedIcon />}
+              icon={<GroupIcon />}
               selected={selected}
               setSelected={setSelected}
             />
             <Item
               title="Messages"
               to="/Messages"
-              icon={<ReceiptOutlinedIcon />}
+              icon={<ForumIcon />}
               selected={selected}
               setSelected={setSelected}
             />
@@ -155,22 +147,21 @@ const SideNavbar = () => {
             <Item
               title="Add Staff"
               to="/form"
-              icon={<PersonOutlinedIcon />}
+              icon={<GroupAddIcon />}
               selected={selected}
               setSelected={setSelected}
             />
             <Box height="200px" />
             <Item
               title="support"
-              to="/calendar"
-              icon={<CalendarTodayOutlinedIcon />}
+              icon={<SupportAgentIcon />}
               selected={selected}
               setSelected={setSelected}
             />
             <Item
               title="Settings"
               to="/faq"
-              icon={<HelpOutlineOutlinedIcon />}
+              icon={<SettingsSuggestIcon />}
               selected={selected}
               setSelected={setSelected}
             />
@@ -193,11 +184,9 @@ const SideNavbar = () => {
               margin: '0 auto',
             }}
           />
-          <Box textAlign="left">
-            <Typography variant="subtitle1">John Doe</Typography>
-            {' '}
-            <Typography variant="body2">john.doe@example.com</Typography>
-            {' '}
+          <Box textAlign="left" marginLeft="16px">
+            <Typography variant="subtitle1">Karan Jain</Typography>
+            <Typography variant="body2">karanjain2212@gmail.com</Typography>
           </Box>
         </Box>
       </Sidebar>
